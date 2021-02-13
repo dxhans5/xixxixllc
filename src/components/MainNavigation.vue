5d03aa15cc10de3884186167a404a7063bea970d
Donny Hansen <dxhans5@gmail.com>
2021-02-13 03:59:45 -0700

    Initial commit

---
+++ src/components/MainNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- Must have the app property -->
+  <v-app-bar app elevate-on-scroll>
+
+    <router-link :to="{ name: 'home'}">
+
+      <v-toolbar-title>xixxix, llc</v-toolbar-title>
+
+    </router-link>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'MainNavigation',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
